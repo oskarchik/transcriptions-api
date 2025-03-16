fda938da6c663a703e8aab32b7e01119b86878ee
oscar <osanzce@gmail.com>
2025-03-16 15:59:25 +0100

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import cors from 'cors'
 import helmet from 'helmet';
 import router from './routes';
 import { errorHandler } from './middlewares/error';
@@ -10,6 +11,15 @@ const VERSION = process.env.VERSION;
 export function createApp() {
   const app = express();
 
+  const corsOptions = {
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    
+  }
+
+  app.use(cors(corsOptions))
   app.use(helmet());
   app.use(express.json());
 
